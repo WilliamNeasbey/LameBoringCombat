6b6bb3bc94b3b8063bb0750ea1c6f514d82ad0c4
WilliamNeasbey <108777720+WilliamNeasbey@users.noreply.github.com>
2023-10-13 14:57:12 +1100

    Added enemu slider and fixed special move hightlight to be more obvious

---
+++ Assets/Scripts/EnemyHealthSlider.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthSlider : MonoBehaviour
+{
+    public Slider healthSlider; // Reference to your UI Slider
+    public float maxHealth = 100f; // The maximum health value (should match the max health of your player)
+
+    // Reference to the script containing the player's health variable
+    public EnemyScript EnemyHealthScript;
+
+    private void Start()
+    {
+        // Ensure the healthSlider and playerHealthScript references are set correctly
+        if (healthSlider == null || EnemyHealthScript == null)
+        {
+            Debug.LogError("Ensure that healthSlider and playerHealthScript references are set in the Inspector.");
+            return;
+        }
+
+        // Set the maximum value of the Slider to match the player's max health
+        healthSlider.maxValue = maxHealth;
+    }
+
+    private void Update()
+    {
+        // Update the Slider's value to match the player's current health
+        healthSlider.value = EnemyHealthScript.health;
+    }
+}
