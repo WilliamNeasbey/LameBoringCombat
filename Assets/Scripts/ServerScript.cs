feb3f41e671c321bca892c67c440fb09218d5078
MicroSillyWilly <1025946@student.sae.edu.au>
2023-10-16 18:02:26 +1100

    botched network implimentation

---
+++ Assets/Scripts/ServerScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
+
+public class ServerScript : MonoBehaviour
+{
+    [SerializeField] private string gameplaySceneName = "CharacterSelect";
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+        //  NetworkManager.Singleton.SceneManager.LoadScene(gameplaySceneName,LoadSceneMode.Single);
+
+    }
+
+    public void StartServer()
+    {
+        NetworkManager.Singleton.StartServer();
+        NetworkManager.Singleton.SceneManager.LoadScene(gameplaySceneName, LoadSceneMode.Single);
+
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
