7cd08aeb2ac402c1a61d99d2a515f16d40f30d17
MicroSillyWilly <1025946@student.sae.edu.au>
2023-10-26 12:31:54 +1100

    sort of fixed lock on

--- Assets/Scripts/LockOnUI.cs
+++ Assets/Scripts/LockOnUI.cs
@@ -4,6 +4,9 @@ public class LockOnUI : MonoBehaviour
 {
     private Transform target; // The locked-on target's transform
 
+    public Vector3 positionOffset = new Vector3(0, 0, 0); // Offset from the target's position
+    public Vector3 lookAtOffset = new Vector3(0, 0, 0); // Offset for making the UI face the camera
+
     // Assign the target (locked-on enemy) for this UI element
     public void SetTarget(Transform newTarget)
     {
@@ -15,11 +18,11 @@ public class LockOnUI : MonoBehaviour
     {
         if (target != null)
         {
-            // Update the UI position to match the locked-on target's position
-            transform.position = target.position;
+            // Update the UI position with the offset
+            transform.position = target.position + positionOffset;
 
-            // Make the UI always face the camera
-            transform.LookAt(Camera.main.transform);
+            // Make the UI always face the camera with the offset
+            transform.LookAt(Camera.main.transform.position + lookAtOffset);
         }
     }
 }
